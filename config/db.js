6fa2ef7a63f7b3c4d0705930eb86f3134d6e31bb
esthefaniav20 <84297019+EsthefaniaVillacres@users.noreply.github.com>
2026-01-12 23:30:10 -0500

    fix: use Railway MySQL env vars

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2/promise");
+
+const db = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT),
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = db;
